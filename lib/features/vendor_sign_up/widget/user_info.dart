2bb2403b5a91e9922fc7e1cc42f8200f2c32cec1
nehal <nehalelsamoly123@gmial.com>
2024-08-06 17:15:56 +0300

    nono22

--- lib/features/vendor_sign_up/widget/user_info.dart
+++ lib/features/vendor_sign_up/widget/user_info.dart
@@ -85,7 +85,7 @@ class _UserInfoWidgetState extends State<UserInfoWidget> {
                           });
                         },
                       ),
-                      //
+                      ////
                     ),
                   ),
                   Container(
